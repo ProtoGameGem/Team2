b8c15d77afa49fe0b9edd12cc8d6ff6e65ab9542
HyukinKwon <901po3@gmail.com>
2021-03-22 23:58:04 +0900

    저장

---
+++ Team2Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    bool IsCreditMenuOn = false;
+    bool IsTutorialPanelOn = false;
+    bool Pressed = false;
+
+    [SerializeField] GameObject CreditPanel;
+    [SerializeField] GameObject TutorialPaenl;
+
+    private void Start()
+    {
+        CreditPanel.SetActive(false);
+        TutorialPaenl.SetActive(false);
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("game_map1");
+    }
+
+    public void OpenCreaditMenu()
+    {
+        CreditPanel.SetActive(true);
+        IsCreditMenuOn = true;
+    }
+
+    public void OpenTutorialMenu()
+    {
+        TutorialPaenl.SetActive(true);
+        IsTutorialPanelOn = true;
+    }
+
+
+    private void Update()
+    {
+        if (Pressed)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Pressed = false;
+            }
+        }
+
+        if (!IsCreditMenuOn && !IsTutorialPanelOn)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Pressed = true;
+            }
+        }
+
+        if (!Pressed)
+        {
+            if (IsCreditMenuOn)
+            {
+                if (Input.GetMouseButtonUp(0))
+                {
+                    IsCreditMenuOn = false;
+                    CreditPanel.SetActive(false);
+                }
+            }
+            else if (IsTutorialPanelOn)
+            {
+                IsTutorialPanelOn = false;
+                TutorialPaenl.SetActive(false);
+            }
+        }
+    }
+}
